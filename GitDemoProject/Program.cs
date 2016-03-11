78268651c4c64c91646ee1fcd6b92e105964d3b0
anhnguyen <dinhanhvnn@gmail.com>
2016-03-11 18:18:52 +0700

    Add Validation Code (Example)

--- GitDemoProject/Program.cs
+++ GitDemoProject/Program.cs
@@ -11,6 +11,12 @@ namespace GitDemoProject
         static void Main(string[] args)
         {
             //Initial Project
+            //Truong hop bạn chua login tài khoản Git trong visual thì sẽ được hỏi để nhập User và pass login vào Git
+            //Trường hợp hiện tại tôi đã login vào tài khoản Git trong VS
+            //Project đã được Push lên Git, vào Git để kiểm tra
+            //Sau khi push lên Git, nếu sau này code có sự thay đổi nào thì sẽ tiến hành Commit lên Git
+
+            //Add more your code
         }
     }
 }
